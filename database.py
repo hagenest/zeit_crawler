12d7de6c3c462232b80d7faef7676ab98d43dbb0
Christian Hagenest <c.hagenest@pm.me>
2021-07-17 15:09:29 +0200

    [refactor] Setup method for database

--- database.py
+++ database.py
@@ -2,14 +2,17 @@ from peewee import *
 import datetime
 
 
+# Create a peewee database instance
 db = SqliteDatabase("leitmedien.db", pragmas={"foreign_keys": 1})
 
 
+# Define a base model class that specifies which database to use.
 class BaseModel(Model):
     class Meta:
         database = db
 
 
+# Define database table for articles
 class Article(BaseModel):
 
     url = TextField(primary_key=True)
@@ -20,15 +23,22 @@ class Article(BaseModel):
     summary = TextField()
 
 
+# Define database table for keywords
 class Keyword(BaseModel):
     article = ForeignKeyField(Article, backref="keywords")
     ykeyword = TextField()
 
-
+# Define database table for authors
 class Author(BaseModel):
     article = ForeignKeyField(Article, backref="authors")
     author = TextField()
 
 
-db.connect()
-db.create_tables([Article, Keyword, Author])
+def setup_database():
+    db.connect()
+    db.create_tables([Article, Keyword, Author])
+    db.close()
+
+
+if __name__ == "__main__":
+    setup_database()
